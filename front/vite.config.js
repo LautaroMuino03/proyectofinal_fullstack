472e2fa9ca73241811a9ce75c4b8629db4fce0f5
Lautaro Muiño <lautaromuino2003@gmail.com>
2024-11-30 17:00:26 -0300

    cambios port

--- front/vite.config.js
+++ front/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server: {
     host: "0.0.0.0",
-    port: 3002,
+    port: 10000,
     },
   base: '/proyectofinal_UTN/',
   plugins: [react()],
