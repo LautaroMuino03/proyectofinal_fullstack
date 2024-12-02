d3778be6385143a464b6414997813b6f938f09a4
Lautaro Muiño <lautaromuino2003@gmail.com>
2024-12-02 11:51:06 -0300

    cambios server

--- back/server.js
+++ back/server.js
@@ -68,7 +68,7 @@ app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(session)
 
 // Configuración del puerto
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT;
 // Iniciar el servidor
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en puerto ${PORT}`);
